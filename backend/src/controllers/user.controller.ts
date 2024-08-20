f7cebc829e03f4c166c3f8edb6017116f8782714
whonix2904 <sahilmachhi29@gmail.com>
2024-08-20 22:56:59 +0530

    added accesstoken refreshtoken generator function

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,5 +1,40 @@
 import { Request, Response } from "express";
 import { User } from "../models/user.model";
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+import { Document } from "mongoose";
+
+const generateAccessRefreshToken = (
+  id: string,
+  email: string,
+  username: string
+): any => {
+  const secret: string = process.env.ACCESS_TOKEN_SECRET || "secret";
+
+  const accessToken = jwt.sign(
+    {
+      id,
+      email,
+      username,
+    },
+    secret,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIERY,
+    }
+  );
+
+  const refreshToken = jwt.sign(
+    {
+      id,
+    },
+    secret,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_SECRET,
+    }
+  );
+
+  return { accessToken, refreshToken };
+};
 
 export const createUser = async (req: Request, res: Response) => {
   try {
@@ -16,7 +51,23 @@ export const createUser = async (req: Request, res: Response) => {
       });
     }
 
-    const newUser = await User.create({ email, username, password, fullname });
+    const saltRounds = 10;
+    const hasedPassword = await bcrypt.hash(password, saltRounds);
+    if (!hasedPassword) {
+      return res.status(404).json({
+        success: false,
+        message: `password hash failed`,
+      });
+    }
+
+    console.log(hasedPassword);
+
+    const newUser = await User.create({
+      email,
+      username,
+      password: hasedPassword,
+      fullname,
+    });
 
     return res.status(200).json({ success: true, message: newUser });
   } catch (error) {
@@ -28,6 +79,73 @@ export const createUser = async (req: Request, res: Response) => {
 };
 
 export const loginUser = async (req: Request, res: Response) => {
-  const logindata = req.body;
-  return res.status(200).json(logindata);
+  try {
+    const { username, email, password } = req.body;
+
+    if (!(username || email)) {
+      return res.status(401).json({
+        success: false,
+        message: `username is already exist`,
+      });
+    }
+
+    interface userObj extends Document {
+      password: string;
+      _id: string;
+      email: string;
+      username: string;
+      refreshToken: string;
+    }
+
+    const user: userObj | null = await User.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (!user) {
+      return res.status(402).json({
+        success: false,
+        message: `user has not created account`,
+      });
+    }
+
+    const secret: string = process.env.ACCESS_TOKEN_SECRET || "secret";
+    const isCorrectPassword = jwt.verify(password, secret);
+
+    if (!isCorrectPassword) {
+      return res.status(406).json({
+        success: false,
+        message: `invalid password`,
+      });
+    }
+
+    const { accessToken, refreshToken } = generateAccessRefreshToken(
+      user._id,
+      user.email,
+      user.username
+    );
+
+    user.refreshToken = refreshToken;
+
+    user.save();
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    res
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .status(200)
+      .json({
+        success: true,
+        message: `user logged in successfully`,
+        data: user,
+      });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: `server error`,
+    });
+  }
 };
