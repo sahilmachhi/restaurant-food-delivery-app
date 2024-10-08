997521cf835c8a5ae861cd34152042cde9cd7b08
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-10-08 21:26:41 +0530

    updateRestaurant and searchRestuarnat functionality added

--- backend/src/controllers/restaurant.controller.ts
+++ backend/src/controllers/restaurant.controller.ts
@@ -139,3 +139,80 @@ export const getRestaurantOrder = async (req: userRequest, res: Response) => {
     });
   }
 };
+
+export const updateRestaurantOrder = async (req: any, res: Response) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    const order = await Order.findOne(orderId);
+
+    if (!order) {
+      return res.status(401).json({
+        success: false,
+        message: "order does not exist",
+      });
+    }
+
+    order.status = status;
+
+    await order.save();
+    return res.status(200).json({
+      success: true,
+      message: "order status updated successful",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "server error",
+    });
+  }
+};
+
+export const searchRestaurant = async (req: any, res: Response) => {
+  try {
+    const searchText: string = req.params.searchText || "";
+    const searchQuery: string = req.params.searchQuery || "";
+    // this code should be reviewed
+    const selectedCuisines = ((req.params.selectedCuisines as string) || "")
+      .split(",")
+      .filter((cuisine) => cuisine);
+
+    const Query: any = {};
+
+    if (searchText) {
+      Query.$or = [
+        {
+          restaurantName: { $regex: searchText, $options: "i" },
+          city: { $regix: searchText, $options: "i" },
+          country: { $regix: searchText, $options: "i" },
+        },
+      ];
+    }
+
+    if (searchQuery) {
+      Query.$or = [
+        {
+          restaurantName: { $regex: searchText, $options: "i" },
+          cuisines: { $regix: searchQuery, $options: "i" },
+        },
+      ];
+    }
+
+    if (selectedCuisines.length > 0) {
+      Query.cuisines = { $in: selectedCuisines };
+    }
+
+    const restuarants = await Restaurant.find(Query);
+
+    return res.status(200).json({
+      success: true,
+      restuarants,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "server error",
+    });
+  }
+};
