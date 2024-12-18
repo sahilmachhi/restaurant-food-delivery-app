f795f17dc941b7ad3a5ad097fe03616e0656cdd7
Sahil <113676978+whonix2904@users.noreply.github.com>
2024-12-18 20:53:50 +0530

    commit

--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -1,11 +1,14 @@
+import { CookieOptions } from "express";
 const expiryDate = new Date();
 expiryDate.setDate(expiryDate.getDate() + 15);
 
-export const CookiesOptions = {
-  //   sameSite: process.env.SAMESITE || "none",
+export const CookiesOptions: CookieOptions = {
+  sameSite: "none",
   //   httpOnly: true,
   httpOnly: process.env.HTTPSONLY === "true",
   //   secure: true,
   secure: process.env.SECURE === "true",
   expires: expiryDate,
+  domain: process.env.FRONTEND_URI || "*",
+  path: "/",
 };
