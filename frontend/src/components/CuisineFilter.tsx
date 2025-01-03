4f4a2f5a9dc3ab8604727c7e23039f24dc298a64
sahilmachhi29 <sahilmachhi29@gmail.com>
2025-01-03 13:57:47 +0000

    minor changes

--- frontend/src/components/CuisineFilter.tsx
+++ frontend/src/components/CuisineFilter.tsx
@@ -1,8 +1,8 @@
 import { cuisineList } from "@/utils/constants";
-import { Check, ChevronDown, ChevronUp } from "lucide-react";
+import {  ChevronDown, ChevronUp } from "lucide-react";
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
-import { Badge } from "./ui/badge";
+
 import { Checkbox } from "./ui/checkbox";
 
 const CuisineFilter = ({
@@ -16,59 +16,9 @@ const CuisineFilter = ({
   isExpanded: boolean;
   onExpandedClick: any;
 }) => {
-  const handleCuisinesChange = (e: any) => {
-    const clickedCuisine = e.target.value;
-    const isChecked = e.target.checked;
-    const newCuisinesList = isChecked
-      ? [...selectedCuisines, clickedCuisine]
-      : selectedCuisines.filter((cuisine: any) => cuisine !== clickedCuisine);
-
-    onChange(newCuisinesList);
-  };
-
   const handleCuisinesReset = () => onChange([]);
-
-  // return (
-  //   <>
-  //     <div className="flex flex-col gap-3 md:flex-row md:items-center md:gap-2 my-3">
-  //       <h1 className="font-medium text-lg">
-  //         {/* ({searchedRestaurant?.data.length}) Search result found */}
-  //       </h1>
-  //       <div className="flex flex-wrap gap-2 mb-4 md:mb-0">
-  //         {cuisineList.map((selectedFilter: string, idx: number) => (
-  //           <div
-  //             key={idx}
-  //             className="relative inline-flex items-center max-w-full"
-  //           >
-  //             <Badge
-  //               className="text-[#D19254] rounded-md hover:cursor-pointer pr-6 whitespace-nowrap"
-  //               variant="outline"
-  //             >
-  //               {selectedFilter}
-  //             </Badge>
-  //             <Button
-  //               // onClick={() => setAppliedFilter(selectedFilter)}
-  //               className="absolute text-[#D19254] right-1 hover:cursor-pointer"
-  //             />
-  //           </div>
-  //         ))}
-  //       </div>
-  //     </div>
-  //   </>
-  // );
-
   return (
     <>
-      {/* <div className="flex justify-between items-center px-2">
-        <div className="text-md font-semibold mb-2">Filter By Cuisine</div>
-        <div
-          onClick={handleCuisinesReset}
-          className="text-sm font-semibold mb-2 underline cursor-pointer text-blue-500"
-        >
-          Reset Filters
-        </div>
-      </div> */}
-
       <div className="">
         <div className="flex items-center justify-between gap-5">
           <h1 className="font-medium text-lg whitespace-nowrap">
@@ -79,53 +29,6 @@ const CuisineFilter = ({
           </Button>
         </div>
 
-        {/* <div className="space-y-2 flex flex-col">
-        {cuisineList
-          .slice(0, isExpanded ? cuisineList.length : 7)
-          .map((cuisine, index) => {
-            const isSelected = selectedCuisines.includes(cuisine);
-            return (
-              <div className="flex" key={index}>
-                <input
-                  id={`cuisine_${cuisine}`}
-                  type="checkbox"
-                  className="hidden"
-                  value={cuisine}
-                  checked={isSelected}
-                  onChange={handleCuisinesChange}
-                />
-                <Label
-                  htmlFor={`cuisine_${cuisine}`}
-                  className={`flex flex-1 items-center cursor-pointer text-sm rounded-full px-4 py-2 font-semibold ${
-                    isSelected
-                      ? "border border-green-600 text-green-600"
-                      : "border border-slate-300"
-                  }`}
-                >
-                  {isSelected && <Check size={20} strokeWidth={3} />}
-                  {cuisine}
-                </Label>
-              </div>
-            );
-          })}
-
-        <Button
-          onClick={onExpandedClick}
-          variant="link"
-          className="mt-4 flex-1"
-        >
-          {isExpanded ? (
-            <span className="flex flex-row items-center">
-              View Less <ChevronUp />
-            </span>
-          ) : (
-            <span className="flex flex-row items-center">
-              View More <ChevronDown />
-            </span>
-          )}
-        </Button>
-      </div> */}
-        {/* <div className="flex flex-col"> */}
         {cuisineList
           .slice(0, isExpanded ? cuisineList.length : 7)
           .map((cuisine, i) => {
@@ -133,11 +36,22 @@ const CuisineFilter = ({
             return (
               <div key={i} className="flex items-center space-x-2 my-4">
                 <Checkbox
-                  id={`cuisine_${cuisine}`}
+                  id={cuisine}
                   checked={isSelected}
-                  onClick={handleCuisinesChange}
+                  onCheckedChange={() => {
+                    const updatedCuisines = isSelected
+                      ? selectedCuisines.filter(
+                        (selectedCuisine: any) =>
+                          selectedCuisine !== cuisine
+                      )
+                      : [...selectedCuisines, cuisine];
+                    onChange(updatedCuisines);
+                  }}
                 />
-                <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                <Label
+                  htmlFor={cuisine}
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
                   {cuisine}
                 </Label>
               </div>
--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { getAllRestaurants, searchRestaurant } from "@/utils/restaurnatApi";
 import { useEffect, useState } from "react";
@@ -6,9 +7,7 @@ import CuisineFilter from "./CuisineFilter";
 import SearchBar from "./SearchBar";
 import SearchResultInfo from "./SearchResultInfo";
 import SortOptionDropdown from "./SortOptionDropdown";
-import { CardFooter } from "./ui/card";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
+
 
 export type searchState = {
   searchText: string;
@@ -101,14 +100,6 @@ const HomePage = () => {
     getRestaurants();
   }, []);
 
-  // if (isLoading) {
-  //   return (
-  //     <>
-  //       <span>loading</span>
-  //     </>
-  //   );
-  // }
-
   if (isError) {
     return (
       <>
@@ -116,123 +107,7 @@ const HomePage = () => {
       </>
     );
   }
-
-  // if (restaurants.length < 1) {
-  //   return (
-  //     <>
-  //       <h1>no restaurants found</h1>
-  //     </>
-  //   );
-  // }
-
-  // return (
-  //   <div className="max-w-7xl mx-auto my-10">
-  //     <div className="flex flex-col md:flex-row justify-between gap-10">
-  //       {/* <FilterPage /> */}
-  //       <div className="flex-1">
-  //         {/* Search Input Field  */}
-  //         <div className="flex items-center gap-2">
-  //           <Input
-  //             type="text"
-  //             // value={searchQuery}
-  //             placeholder="Search by restaurant & cuisines"
-  //             // onChange={(e) => setSearchQuery(e.target.value)}
-  //           />
-  //           <Button
-  //             // onClick={() =>
-  //             //   searchRestaurant(params.text!, searchQuery, appliedFilter)
-  //             // }
-  //             className="bg-orange hover:bg-hoverOrange"
-  //           >
-  //             Search
-  //           </Button>
-  //         </div>
-  //         {/* Searched Items display here  */}
-  //         <div className="flex flex-col">
-  //           <CuisineFilter
-  //             selectedCuisines={searchState.selectedCuisines}
-  //             onChange={setSelectedCuisines}
-  //             isExpanded={isExpanded}
-  //             onExpandedClick={() =>
-  //               setIsExpanded((prevIsExpanded) => !prevIsExpanded)
-  //             }
-  //           />
-  //           {/* Restaurant Cards  */}
-  //           {/* <div className="grid md:grid-cols-3 gap-4">
-  //             {loading ? (
-  //               <SearchPageSkeleton />
-  //             ) : !loading && searchedRestaurant?.data.length === 0 ? (
-  //               <NoResultFound searchText={params.text!} />
-  //             ) : (
-  //               searchedRestaurant?.data.map((restaurant: Restaurant) => (
-  //                 <Card
-  //                   key={restaurant._id}
-  //                   className="bg-white dark:bg-gray-800 shadow-xl rounded-xl overflow-hidden hover:shadow-2xl transition-shadow duration-300"
-  //                 >
-  //                   <div className="relative">
-  //                     <AspectRatio ratio={16 / 6}>
-  //                       <img
-  //                         src={restaurant.imageUrl}
-  //                         alt=""
-  //                         className="w-full h-full object-cover"
-  //                       />
-  //                     </AspectRatio>
-  //                     <div className="absolute top-2 left-2 bg-white dark:bg-gray-700 bg-opacity-75 rounded-lg px-3 py-1">
-  //                       <span className="text-sm font-medium text-gray-700 dark:text-gray-300">
-  //                         Featured
-  //                       </span>
-  //                     </div>
-  //                   </div>
-  //                   <CardContent className="p-4">
-  //                     <h1 className="text-2xl font-bold text-gray-900 dark:text-gray-100">
-  //                       {restaurant.restaurantName}
-  //                     </h1>
-  //                     <div className="mt-2 gap-1 flex items-center text-gray-600 dark:text-gray-400">
-  //                       <MapPin size={16} />
-  //                       <p className="text-sm">
-  //                         City:{" "}
-  //                         <span className="font-medium">{restaurant.city}</span>
-  //                       </p>
-  //                     </div>
-  //                     <div className="mt-2 gap-1 flex items-center text-gray-600 dark:text-gray-400">
-  //                       <Globe size={16} />
-  //                       <p className="text-sm">
-  //                         Country:{" "}
-  //                         <span className="font-medium">
-  //                           {restaurant.country}
-  //                         </span>
-  //                       </p>
-  //                     </div>
-  //                     <div className="flex gap-2 mt-4 flex-wrap">
-  //                       {restaurant.cuisines.map(
-  //                         (cuisine: string, idx: number) => (
-  //                           <Badge
-  //                             key={idx}
-  //                             className="font-medium px-2 py-1 rounded-full shadow-sm"
-  //                           >
-  //                             {cuisine}
-  //                           </Badge>
-  //                         )
-  //                       )}
-  //                     </div>
-  //                   </CardContent>
-  //                   <CardFooter className="p-4 border-t dark:border-t-gray-700 border-t-gray-100 text-white flex justify-end">
-  //                     <Link to={`/restaurant/${restaurant._id}`}>
-  //                       <Button className="bg-orange hover:bg-hoverOrange font-semibold py-2 px-4 rounded-full shadow-md transition-colors duration-200">
-  //                         View Menus
-  //                       </Button>
-  //                     </Link>
-  //                   </CardFooter>
-  //                 </Card>
-  //               ))
-  //             )}
-  //           </div> */}
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
+  
   return (
     <>
       <div className="flex items-start w-full justify-between gap-5">
